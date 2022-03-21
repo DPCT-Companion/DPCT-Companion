da38af4660ec1975f180f35bd52d1881af000d48
yyuchenn <13705747+yyuchenn@users.noreply.github.com>
2022-03-21 03:14:27 +0000

    html report

--- tester/run.py
+++ tester/run.py
@@ -1,11 +1,28 @@
 from typing import Dict
 from sys import argv
 import yaml
+from jinja2 import Environment, FileSystemLoader
+import difflib
 
 from Build import build
 from Test import test
 from Profile import profile
 from Clean import clean
+
+def report(result):
+    template = Environment(loader=FileSystemLoader("."), autoescape=True).get_template("report_template.html")
+    for check in result:
+        check.o1 = [i+'\n' for i in check.o1.splitlines()]
+        check.o2 = [i+'\n' for i in check.o2.splitlines()]
+    stat = {
+        "total": len(result),
+        "pass": sum(check.pass_check for check in result),
+        "crashed": sum(check.c1 for check in result),
+        "umatch": sum(not check.pass_check for check in result) - sum(check.c1 for check in result)}
+
+    with open('report.html', 'w' ) as f:
+        f.write(template.render(checks=result, stat=stat, difflib=difflib, list=list))
+
 
 if __name__ == '__main__':
     try:
@@ -41,10 +58,12 @@ if __name__ == '__main__':
         # print(test_cases, cuda_exec, dpcpp_exec)
         result = test(test_cases, cuda_exec, dpcpp_exec)
         # TODO : export to report
+        report(result)
 
         # TODO: Place the timeout value in yaml specification.
         # TODO: Export profiler message to report.
         # Profiler is not enabled right now.
         # profiler_message = profile(dpcpp_exec, 300)
+
         if "clean" in config.keys():
             clean(config["clean"])

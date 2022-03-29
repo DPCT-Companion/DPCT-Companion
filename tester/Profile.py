11202d7f4cd40a149fd7c7cbb6dab9dead7e2a23
Yuyi Liang <asto18089@126.com>
2022-03-29 19:20:35 +0100

    Profiler: Add functionality for profiling CUDA applications.

--- tester/Profile.py
+++ tester/Profile.py
@@ -1,39 +1,60 @@
 """
 Tester module to profile the built executable on GPU.
-Currently, Nsight profilers cannot be run on our machines, only Intel VTune profiler will work.
-Before running it, Intel oneAPI environment must be initialized.
+For NVIDIA CUDA, we use NVIDIA Nsight Compute (NCU), and for DPC++ we use Intel VTune profiler.
+Before running it, Intel oneAPI environment must be initialized and NVIDIA Nsight Compute (NCU) must be installed.
 Profiler is run on SYNC manner with a timeout.
 
 Args:
-    dpcpp_exec (str): The executable name.
+    cuda_exec (str): The CUDA executable name.
+    dpcpp_exec (str): The DPC++ executable name.
     timeout (int): Timeout in seconds to terminate the profiler.
 
 Returns:
     profiler_message (dict: {str, float}):
-        "gpu_time": GPU active time in seconds.
-        "gpu_eu_idle": GPU EU (Execution Unit) idle time versus total possible time in percentage. Indicator of GPU uArch efficiency.
+        "cuda_gpu_time": CUDA GPU active time in seconds.
+        "cuda_gpu_sm_active": NVIDIA GPU SM (Streaming Multiprocessor) active percentage.
+        "dpcpp_gpu_time": DPC++ GPU active time in seconds.
+        "dpcpp_gpu_eu_idle": Intel GPU EU (Execution Unit) active percentage.
 """
 
 import re
 import subprocess
 
 
-def profile(dpcpp_exec: str, timeout: int) -> dict:
+def profile(cuda_exec: str, dpcpp_exec: str, timeout: int) -> dict:
     try:
-        dpcpp_profile_gpu_cmd = "vtune -collect gpu-hotspots {}".format(dpcpp_exec)
-        gpu_time_patt = re.compile(r"GPU Time: (\d+\.\d+)s")
-        gpu_eu_efficiency_patt = re.compile(r"EU Array Stalled/Idle: (\d+\.\d+)%")
+        cuda_profile_gpu_cmd = "ncu {}".format(cuda_exec)
+        cuda_gpu_time_patt = re.compile(r"Duration usecond (\d+\.\d+)")
+        cuda_gpu_sm_active_patt = re.compile(r"Compute \(SM\) \[%] % (\d+\.\d+)")
+        print("Profiling Original CUDA program. Timeout is {} seconds.".format(timeout))
+        result = subprocess.run(cuda_profile_gpu_cmd.split(), timeout=timeout, stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT, encoding="utf-8")
 
+        cuda_profiler_output = result.stdout
+        cuda_gpu_time_list = re.findall(cuda_gpu_time_patt, cuda_profiler_output)
+        cuda_gpu_sm_active_list = re.findall(cuda_gpu_sm_active_patt, cuda_profiler_output)
+        cuda_gpu_time = 0.0
+        for time in cuda_gpu_time_list:
+            cuda_gpu_time += float(time)
+        cuda_gpu_sm_active = 0.0
+        for time in cuda_gpu_sm_active_list:
+            cuda_gpu_sm_active += float(time)
+        cuda_gpu_sm_active /= len(cuda_gpu_sm_active_list)
+
+        dpcpp_profile_gpu_cmd = "vtune -collect gpu-hotspots {}".format(dpcpp_exec)
+        dpcpp_gpu_time_patt = re.compile(r"GPU Time: (\d+\.\d+)s")
+        dpcpp_gpu_eu_efficiency_patt = re.compile(r"EU Array Stalled/Idle: (\d+\.\d+)%")
         print("Profiling ported DPC++ program. Timeout is {} seconds.".format(timeout))
         result = subprocess.run(dpcpp_profile_gpu_cmd.split(), timeout=timeout, stdout=subprocess.PIPE,
                                 stderr=subprocess.STDOUT, encoding="utf-8")
-        profiler_output = result.stdout
+        dpcpp_profiler_output = result.stdout
+        dpcpp_gpu_time = float(re.search(dpcpp_gpu_time_patt, dpcpp_profiler_output).group(1))
+        dpcpp_gpu_eu_active = 100.0 - float(re.search(dpcpp_gpu_eu_efficiency_patt, dpcpp_profiler_output).group(1))
 
-        gpu_time = float(re.search(gpu_time_patt, profiler_output).group(1))
-        gpu_eu_idle = float(re.search(gpu_eu_efficiency_patt, profiler_output).group(1))
-
-        return {"gpu_time": gpu_time,
-                "gpu_eu_idle": gpu_eu_idle}
+        return {"cuda_gpu_time": cuda_gpu_time,
+                "cuda_gpu_sm_active": cuda_gpu_sm_active,
+                "dpcpp_gpu_time": dpcpp_gpu_time,
+                "dpcpp_gpu_eu_active": dpcpp_gpu_eu_active}
 
     except subprocess.TimeoutExpired as e:
         print("Timed out in profiling. Complete output message for the profiler is printed below for reference:\n")
@@ -41,5 +62,7 @@ def profile(dpcpp_exec: str, timeout: int) -> dict:
         print(e.stdout)
         print("\n========stderr========")
         print(e.stderr)
-        return {"gpu_time": 0.0,
-                "gpu_eu_idle": 0.0}
+        return {"cuda_gpu_time": 0.0,
+                "cuda_gpu_sm_active": 0.0,
+                "dpcpp_gpu_time": 0.0,
+                "dpcpp_gpu_eu_active": 0.0}
--- tester/test/test_profiler.py
+++ tester/test/test_profiler.py
@@ -1,13 +1,20 @@
 import unittest
+
 from tester.Profile import *
 
 
 class ProfilerTest(unittest.TestCase):
     def test_profiler(self):
-        result = profile("a.out", 1200)
-        self.assertIsInstance(result["gpu_time"], float)
-        self.assertIsInstance(result["gpu_eu_idle"], float)
-        self.assertGreater(result["gpu_time"], 0.0)
+        # TODO: We need a CUDA / DPC++ application which actually used the GPU here.
+        result = profile("a.out", "b.out", 1200)
+
+        self.assertIsInstance(result["cuda_gpu_time"], float)
+        self.assertIsInstance(result["cuda_gpu_sm_active"], float)
+        self.assertGreater(result["cuda_gpu_time"], 0.0)
+
+        self.assertIsInstance(result["dpcpp_gpu_time"], float)
+        self.assertIsInstance(result["dpcpp_gpu_eu_active"], float)
+        self.assertGreater(result["dpcpp_gpu_time"], 0.0)
 
 
 if __name__ == '__main__':

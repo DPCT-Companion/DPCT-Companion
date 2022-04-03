fc737149de35531bc53df613b3367a0f7060aa8f
Yuyi Liang <asto18089@126.com>
2022-04-03 16:05:31 +0100

    Profiler: Add further tests.

--- tests/test_profiler.py
+++ tests/test_profiler.py
@@ -6,15 +6,16 @@ from tester.Profile import *
 class ProfilerTest(unittest.TestCase):
     def test_profiler(self):
         # TODO: We need a CUDA / DPC++ application which actually used the GPU here.
-        result = profile("a.out", "b.out", "cuda,dpcpp", 1200)
+        result_cuda = profile_cuda("cuda_prof_test", None, 1200)
+        result_dpcpp = profile_dpcpp("dpcpp_prof_test", None, 1200)
 
-        self.assertIsInstance(result["cuda_gpu_time"], float)
-        self.assertIsInstance(result["cuda_gpu_sm_active"], float)
-        self.assertGreater(result["cuda_gpu_time"], 0.0)
+        self.assertIsInstance(result_cuda["cuda_gpu_time"], float)
+        self.assertIsInstance(result_cuda["cuda_gpu_sm_active"], float)
+        self.assertGreater(result_cuda["cuda_gpu_time"], 0.0)
 
-        self.assertIsInstance(result["dpcpp_gpu_time"], float)
-        self.assertIsInstance(result["dpcpp_gpu_eu_active"], float)
-        self.assertGreater(result["dpcpp_gpu_time"], 0.0)
+        self.assertIsInstance(result_dpcpp["dpcpp_gpu_time"], float)
+        self.assertIsInstance(result_dpcpp["dpcpp_gpu_eu_active"], float)
+        self.assertGreater(result_dpcpp["dpcpp_gpu_time"], 0.0)
 
 
 if __name__ == '__main__':

d839dda609411da4fec0065f41caaed25a8b15c1
yyuchenn <13705747+yyuchenn@users.noreply.github.com>
2022-04-04 04:30:03 +0100

    fix warning fixer runner

--- warning_fixer/run.py
+++ warning_fixer/run.py
@@ -1,12 +1,14 @@
-from os import system
 from sys import argv
+from subprocess import run
 
 from warning_fixer.SourceProject import *
 
-system(f"cd data && mkdir final_output && dpct {' '.join(argv[1:])}")
+logPath = "/app/data/dpct_output/log.log"
+with open(logPath) as logFile:
+    run(f"cd data && mkdir final_output && dpct {' '.join(argv[1:])}", shell=True, stdout=logFile, check=True)
 
 project = SourceProject(dpcpp_root_path="/app/data/dpct_output",
                         cuda_root_path="/app/data",
                         output_path="/app/data/final_output",
-                        log_path="/app/data/dpct_output/log.log")
+                        log_path=logPath)
 project.fix_project_warnings()
